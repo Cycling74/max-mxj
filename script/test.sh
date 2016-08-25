9831a6f5de8e426d9acab8352c9a6866c8cd739a
Timothy Place <tim@cycling74.com>
2016-08-25 10:48:46 -0500

    updated version of max-test to be used

--- script/test.sh
+++ script/test.sh
@@ -6,9 +6,9 @@ mkdir -p ~/Documents/Max\ 7/Packages/
 cp -r $PACKAGE_NAME ~/Documents/Max\ 7/Packages/
 
 # 2. Download and copy the Test framework package into the install location
-curl https://github.com/Cycling74/max-test/releases/download/v1.0-beta2/max-test-v1.0-beta2.zip -L --output max-test.zip
+curl https://github.com/Cycling74/max-test/releases/download/v1.0-beta3/max-test-v1.0-beta3.zip -L --output max-test.zip
 unzip max-test.zip -d Packages
-cp -r Packages/max-test-v1.0-beta2 ~/Documents/Max\ 7/Packages/
+cp -r Packages/max-test-v1.0-beta3 ~/Documents/Max\ 7/Packages/
 
 # 3. Download and install Max
 curl http://akiaj5esl75o5wbdcv2a-maxmspjitter.s3.amazonaws.com/Max724_160725.dmg --output max.dmg
@@ -21,7 +21,7 @@ rm -rf ~/Applications/Max.app/Contents/Resources/C74/packages/max-mxj
 cp -rv $PACKAGE_NAME ~/Applications/Max.app/Contents/Resources/C74/packages
 
 # 5. Configure
-cd ~/Documents/Max\ 7/Packages/max-test-v1.0-beta2
+cd ~/Documents/Max\ 7/Packages/max-test-v1.0-beta3
 mv misc/testpackage-config-example.json misc/testpackage-config.json
 #rm -rf patchers
 cd ruby
