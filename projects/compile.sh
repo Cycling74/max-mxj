e20890181ba7fe847903bc3d9d860cd924fed224
Nick Rothwell <nick@cassiel.com>
2016-08-30 10:42:59 +0100

    Qualified name test (and modified compile script for same).

--- projects/compile.sh
+++ projects/compile.sh
@@ -6,11 +6,17 @@
 BOOT_CLASSES=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Classes/classes.jar
 MAX_JAR=/Applications/Max.app/Contents/Resources/C74/packages/max-mxj/java-classes/lib/max.jar
 
-for f in $(find . -name '*.java'); do
-    echo $f ...
-    dir=$(dirname $f)
-    file=$(basename $f)
-    (cd $dir && javac -bootclasspath $BOOT_CLASSES -cp $MAX_JAR -source 6 -target 6 $file)
+for f in $(find . -name '*.maxproj'); do
+    code_root=${f%/*.maxproj}/code
+
+    if [ -d $code_root ]; then
+        echo ROOT: $code_root
+        pushd $code_root
+        sources=`find . -name '*.java'`
+        echo SOURCES: $sources
+        javac -bootclasspath $BOOT_CLASSES -cp $MAX_JAR -source 6 -target 6 $sources
+        popd
+    fi
 done
 
 # However: specifically (re)compile this trap file at higher Java version, to see if we identify it:
