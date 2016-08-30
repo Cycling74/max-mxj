3f2f57015f1f43a07e3f00ba6c4232a7475066c6
Nick Rothwell <nick@cassiel.com>
2016-08-30 10:02:11 +0100

    Auto-build of .maxzips.

--- projects/compile.sh
+++ projects/compile.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Compile all source files under Java 6, OS X.
+# Run in the directory containing this script.
 
 BOOT_CLASSES=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Classes/classes.jar
 MAX_JAR=/Applications/Max.app/Contents/Resources/C74/packages/max-mxj/java-classes/lib/max.jar
@@ -12,7 +13,7 @@ for f in $(find . -name '*.java'); do
     (cd $dir && javac -bootclasspath $BOOT_CLASSES -cp $MAX_JAR -source 6 -target 6 $file)
 done
 
-# However: specifically (re)compile this trap file at higher Java version, to see if we trap it:
+# However: specifically (re)compile this trap file at higher Java version, to see if we identify it:
 
 NEWER=NewerFormat.java
 echo "Compiling later version: " $NEWER
